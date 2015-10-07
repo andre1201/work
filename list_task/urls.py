f9d9aa69b4a6e550d31b30a0cd49ab02b6f3da68
shukin_am <shukin_am@crvtu.local>
2015-10-07 17:36:23 +0300

    new branch

--- list_task/urls.py
+++ list_task/urls.py
@@ -8,7 +8,7 @@ from list_task import views
 urlpatterns = [
     url(r'^$', views.task_list , name = 'task_list'),
     url(r'^(?P<id_task>\d+)/$', views.task_detail , name = 'task_detail'),
-    url(r'^(?P<date>\d{4}-\d{2}-\d{2})/$', views.task_date , name = 'task_date'),
+    url(r'^(?P<date>((\d{4}-\d{2}-\d{2})||(\d{2}:\d{0,2}))||(\d{4}-\d{2}-\d{2} \d{2}:\d{0,2}))/$', views.task_date , name = 'task_date'),
     url(r'^user/$', views.user_list , name = 'user_list'),
     url(r'^user/(?P<id_user>\d+)/$', views.user_detail , name = 'user_detail'),
 ]
