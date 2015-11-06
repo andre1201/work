1eebadae10b2da92aaa1efab89d6c30407928007
shukin_am <shukin_am@crvtu.local>
2015-11-06 13:04:51 +0300

    Реформат кода

--- task/commons/MixinReport.py
+++ task/commons/MixinReport.py
@@ -12,14 +12,14 @@ class MixinReport():
         Использует для рендера шаблонов
             С использование библиотеки secretary
     """
-    engine = Renderer()
+    __engine = Renderer()
     template_name = ''  # Имя шаблона для рендера
     context = {}  # Данные
 
     # renderers
-    def renders(self):
+    def __renders(self):
         template = os.path.join(settings.BASE_DIR, 'static', self.template_name)
-        result = self.engine.render(template, **self.context)
+        result = self.__engine.render(template, **self.context)
         return result
 
 
@@ -28,6 +28,6 @@ class MixinReport():
         """
             Возваращет готовый шаблон
         """
-        response = HttpResponse(content_type='application/odt', content=self.renders())
+        response = HttpResponse(content_type='application/odt', content=self.__renders())
         response['Content-Disposition'] = 'attachment; filename="renders.odt"'
         return response
